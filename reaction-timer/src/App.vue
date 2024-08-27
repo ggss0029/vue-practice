29b4d697cadf4033c0ac5e2d60a402a48e0efb77
ksj <ggss0029@naver.com>
2024-08-27 15:01:43 +0900

    new file:   reaction-timer/.browserslistrc 	new file:   reaction-timer/.gitignore 	new file:   reaction-timer/README.md 	new file:   reaction-timer/babel.config.js 	new file:   reaction-timer/jsconfig.json 	new file:   reaction-timer/package-lock.json 	new file:   reaction-timer/package.json 	new file:   reaction-timer/public/favicon.ico 	new file:   reaction-timer/public/index.html 	new file:   reaction-timer/src/App.vue 	new file:   reaction-timer/src/assets/logo.png 	new file:   reaction-timer/src/components/Block.vue 	new file:   reaction-timer/src/components/Results.vue 	new file:   reaction-timer/src/main.js 	new file:   reaction-timer/vue.config.js

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <h1>반응 속도 게임</h1>
+  <button  @click="start" :disabled="isPlaying">시작</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
+  <!-- <p v-if="showResults">{{ score }}</p> -->
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods : {
+    start() {
+      this.delay = 2000 + Math.random() * 5000  //2000 = 2초
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>반응 속도 - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: [ 'score' ],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250) {
+            this.rank = '오오 빠름!'
+        }else if(this.score < 400) {
+            this.rank = '쏘쏘~'
+        }else {
+            this.rank = '거북이세요?'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
