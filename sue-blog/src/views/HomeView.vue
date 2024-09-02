636725e75cb425c1cc2e8d6dd6f7ae118601b9fd
ksj <ggss0029@naver.com>
2024-09-02 16:32:00 +0900

    Composition API 학습

---
+++ sue-blog/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    home
+    <p>내 이름은 {{ name }} 나이는 {{ age }} 살 이야!</p>
+    <button @click="handleClick">클릭</button>
+    <button @click="age++">1살 추가</button>
+    <input type="text" v-model="name">
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+
+export default {
+  name: 'HomeView',
+  setup() {
+    // const p = ref(null)
+
+    const name = ref('스폰지밥')
+    const age = ref(10)
+
+    const handleClick = () => {
+      name.value = '뚱이'
+      age.value = '20'
+    }
+
+    return { name, age, handleClick}
+  }, 
+}
+</script>
---
+++ sue-blog/src/views/SearchView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1>Search</h1>
+    <input type="text" v-model="search">
+    <p>search - {{ search }}</p>
+    <div v-for="name in matchNames" :key="name">{{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+    name: 'Search',
+    setup() {
+        const search = ref('')
+        const names = ref(['스폰지밥', '뚱이', '징징이', '다람이', '집게사장', '플랭크톤', '핑핑이'])
+
+        const matchNames = computed(() => {
+            return names.value.filter((name) => name.includes(search.value))
+        })
+
+        return {search, names, matchNames}
+    }
+}
+</script>
+
+<style>
+
+</style>
